488d937056ec3f7c99f1ce6860ee9c2432bddd30
mpsido <1979371837@qq.com>
2019-04-01 16:18:17 +0800

    Merging putHero and putHeroReferal functions together

--- contracts/Blocklords.sol
+++ contracts/Blocklords.sol
@@ -102,54 +102,24 @@ function randomFromAddress(address entropy) private view returns (uint8) {
     mapping (uint => Hero) heroes;
 
     event HeroCreation(address creator, uint id);
+    event HeroCreationWithReferalLink(address creator, uint id, address referer_address);
 
-    function putHero(uint id, uint troopsCap, uint leadership,  uint intelligence, uint strength, uint speed, uint defense, uint item1, uint item2, uint item3, uint item4, uint item5) public payable returns(bool){
-            require(msg.value == createHeroFee, "Payment fee does not match");
+    function putHero(uint id, uint referer_id, address referer_address, uint[6] heroStats, uint[5] hero_items) public payable returns(bool){
+
             require(id > 0,
             "Please insert id higher than 0");
             //require(payments[id].PAYER == owner, "Payer and owner do not match");
             require(heroes[id].OWNER == 0x0000000000000000000000000000000000000000,
             "Hero with this id already exists");
 
-            // TODO check item is not for stronghold reward
-            require(items[item1].OWNER != 0x0000000000000000000000000000000000000000, "Item is not exist");
-            require(items[item2].OWNER != 0x0000000000000000000000000000000000000000, "Item is not exist");
-            require(items[item3].OWNER != 0x0000000000000000000000000000000000000000, "Item is not exist");
-            require(items[item4].OWNER != 0x0000000000000000000000000000000000000000, "Item is not exist");
-            require(items[item5].OWNER != 0x0000000000000000000000000000000000000000, "Item is not exist");
-
-            //delete payments[id]; // delete payment hash after the hero was created in order to prevent double spend
-            heroes[id] = Hero(msg.sender, troopsCap, leadership,  intelligence, strength, speed, defense);
-
-            items[item1].OWNER = msg.sender;
-            items[item2].OWNER = msg.sender;
-            items[item3].OWNER = msg.sender;
-            items[item4].OWNER = msg.sender;
-            items[item5].OWNER = msg.sender;
-
-            emit HeroCreation(msg.sender, id);
-            return true;
-    }
-    event ReferalLink(address referer, uint id, address refered);
-
-    function putHeroReferal(uint referer_id, address referer_address, uint id, uint[6] heroStats, uint[5] hero_items) public payable returns(bool){
-
-            require(id > 0,
-            "Please insert id higher than 0");
-
-            require(referer_id > 0,
-            "Please insert referer_id higher than 0");
-            //require(payments[id].PAYER == owner, "Payer and owner do not match");
-            require(heroes[id].OWNER == 0x0000000000000000000000000000000000000000,
-            "Hero with this id already exists");
-
-            require(heroes[referer_id].OWNER == referer_address,
-            "The referer is not in the game");
-
             require(msg.value == createHeroFee, "Payment fee does not match");
-            require(referer_address.send(referalReward), "Could not send referal reward");
+            if (referer_id > 0) {
+                require(heroes[referer_id].OWNER == referer_address,
+                "The referer is not in the game");
+                require(referer_address.send(referalReward), "Could not send referal reward");
+                emit HeroCreationWithReferalLink(msg.sender, id, referer_address);
+            }
 
-            emit ReferalLink(referer_address, referer_id, msg.sender);
             // TODO check item is not for stronghold reward
             require(items[hero_items[0]].OWNER != 0x0000000000000000000000000000000000000000, "Item does not exist");
             require(items[hero_items[1]].OWNER != 0x0000000000000000000000000000000000000000, "Item does not exist");
@@ -166,6 +136,7 @@ function randomFromAddress(address entropy) private view returns (uint8) {
             items[hero_items[3]].OWNER = msg.sender;
             items[hero_items[4]].OWNER = msg.sender;
 
+            emit HeroCreation(msg.sender, id);
 
             return true;
     }
